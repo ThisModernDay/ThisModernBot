1bc90f00295936f690e8f141341924b4d2105b0a
BrandonLeffew <pls.brcleffew@gmail.com>
2018-04-14 07:07:54 -0400

    added clear to quotes and a return for no quotes

--- commands/quote.js
+++ commands/quote.js
@@ -1,14 +1,20 @@
 module.exports.run = (bot, db, writeData, chan, user, msg, cmd, args) => {
   if(db[user.username].quotes.length > 4){
-    db[user.username].quotes.length = 5;
+    db[user.username].quotes.length = 4;
   }
-  if(args.length === 1){
-    db[user.username].quotes.unshift(db[user.username].lastSent)
+  if(args[0] === "me" ){
+    db[user.username].quotes.unshift(db[user.username].lastSent);
   }
-  if(args.length === 0){
-    bot.action(chan, `${db[user.username].quotes[Math.floor(Math.random() * db[user.username].quotes.length)]} - ${user.username}`)
+  if(args[0] === "clear"){
+    db[user.username].quotes = [];
   }
-  writeData(db)
+  if(db[user.username].quotes.length === 0){
+    bot.action(chan, `${user.username} you don't have any quotes :(`)
+  }
+  if(args.length === 0 && db[user.username].quotes.length > 0){
+    bot.action(chan, `${db[user.username].quotes[Math.floor(Math.random() * db[user.username].quotes.length)]} - ${user.username}`);
+  }
+  writeData(db);
 }
 
 module.exports.help = {
